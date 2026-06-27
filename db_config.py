977bb1df04a9f3f63faa67a48c74710416d595b3
SunidhiGirish <sunidhigirishk@gmail.com>
2026-06-27 17:08:51 +0530

    Pharmacy Hub project

--- db_config.py
+++ db_config.py
@@ -4,6 +4,6 @@ def get_db_connection():
     return mysql.connector.connect(
         host='localhost',
         user='root',
-        password='Chintu@2238',           # your MySQL password
+        password='password',           # your MySQL password
         database='pharmacy_hubs' # your Workbench DB name
     )
